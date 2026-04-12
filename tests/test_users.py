d665ba77ec4d7dfefaac8ef0842207bff7b8ef59
Jenny Saucy <jenny.saucy@outlook.com>
2026-04-12 09:56:08 +0700

    Test: add delete non existint user test, fix admin typo

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,78 +1,73 @@
 #-----------------------------------------------------
-# Tester le métier sur les users
+# Tester on USERS
 #-----------------------------------------------------
 
 # Test GET /users
 # -----------------------------------------------------
-# Test GET /users - Un admin peut lister les users
+# An administrator can list the users
 def test_get_users_ok(client, admin_token):
-    headers = {"Authorization": f"Bearer {admin_token}"}
-
     response = client.get(
         "/users",
-        headers = headers
+        headers = {"Authorization": f"Bearer {admin_token}"}
     )
-
     assert response.status_code == 200
 
 # Test GET /users/{user_id}
 #-----------------------------------------------------
-# Test GET - Un admin peut lire un user
+# An admin can view a user
 def test_get_users_with_user_id_ok(client, admin_token, user_id):
-    headers = {"Authorization": f"Bearer {admin_token}"}
-
     response = client.get(
         f"/users/{user_id}",
-        headers=headers
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
 
     assert response.status_code == 200
 
-# Test GET - Si user inexistant - Impossible de le lire
+# User does not exist -> Cannot read it
 def test_get_users_no_existing_user_ko(client, admin_token):
-    headers = {"Authorization": f"Bearer {admin_token}"}
-
     response = client.get(
         f"/users/{400}",
-        headers=headers
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
 
     assert response.status_code == 404
 
 # Test PUT /users/{user_id}
 #----------------------------------------------------
-# PUT /users/{user_id} - admin peut modifier
+# An admin can update
 def test_put_users_ok(client, admin_token, user_id):
-    headers = {"Authorization": f"Bearer {admin_token}"}
-
     response = client.put(
         f"/users/{user_id}",
         json ={"username": "Jenifer"},
-        headers = headers
+        headers = {"Authorization": f"Bearer {admin_token}"}
     )
     assert response.status_code == 200
 
-# PUT /users/{user_id} - Si user inexistant - Impossible de le modifier
+# User does not exist -> Impossible to update
 def test_put_user_non_existing_user_ko (client, admin_token):
-    headers = {"Authorization": f"Bearer {admin_token}"}
-
     response = client.put(
         f"/users/{44}",
         json={"username": "Jenifer"},
-        headers=headers
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
     assert response.status_code == 404
     assert response.json()["detail"] == "User not found"
 
 # DELETE /users/{user_id}
 # --------------------------------------------------
-# DELETE /users/{user_id} - Un admin peut supprimer un user
+# An admin can delete a user
 def test_delete_a_user_ok (client, admin_token, user_id):
-    headers = {"Authorization": f"Bearer {admin_token}"}
-
     response = client.delete(
         f"/users/{user_id}",
-        headers = headers
+        headers = {"Authorization": f"Bearer {admin_token}"}
     )
 
     assert response.status_code == 200
+
+# Cannot delete a non existing user
+def test_delete_non_existing_user_ko(client, admin_token):
+    response = client.delete(
+        f"/users/{44}",
+        headers={"Authorization": f"Bearer {admin_token}"}
+    )
+    assert response.status_code == 404
